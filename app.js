74d319d55a88a35fb3ffc4803cc5309d5c005102
Chiara888 <carolin@family-klose.de>
2023-05-30 18:19:53 +0200

    final push

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
- 
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + "/views/home-page.html");
+});
+
+app.get("/about-page", (req, res, next) => {
+    res.sendFile(__dirname + "/views/about-page.html");
+});
+
+app.get("/works-page", (req, res, next) => {
+    res.sendFile(__dirname + "/views/works-page.html");
+});
+
+app.get("/photo-gallery", (req, res, next) => {
+    res.sendFile(__dirname + "/views/photo-gallery.html");
+});
+
+app.listen(3000, () => {
+    console.log("start using my port please")
+});
